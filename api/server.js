0600d04881b2fe9a0f254f4d1a68ed3291854733
Daniel Huong <daniel.huong86@gmail.com>
2017-12-06 11:38:27 +1100

    initial comit

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const server = express()
+
+// Plugins
+server.use(bodyParser.json()) // Allows me to have JSPn uploads (POST/PUT/PATCH)
+
+// Routes
+
+// Start the server
+server.listen(7000, (error) => {
+  if (error) {
+    console.error('Error starting', error)
+  }
+  else {
+    console.log('Started at http://localhost:7000')
+  }
+})
